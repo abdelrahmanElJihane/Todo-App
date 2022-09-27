df1572d438af9068ee86db9985cd0f5c616f614e
abdelrahmanElJihane <abdelrahmanocasha9@gmail.com>
2022-09-27 13:38:18 -0700

    second commit

--- lib/layout/home_layout_screen.dart
+++ lib/layout/home_layout_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_5/modules/archived_tasks_screen/archived_tasks_screen.dart';
 import 'package:flutter_application_5/modules/done_tasks_screen/done_tasks_screen.dart';
+import 'package:sqflite/sqflite.dart';
 
 import '../modules/new_tasks_screen/new_tasks_screen.dart';
 
@@ -24,6 +25,11 @@ class _HomeLayoutScreenState extends State<HomeLayoutScreen> {
     'Done Tasks',
     'Archived Tasks',
   ];
+  @override
+    void initState(){
+    super.initState();
+    createDatabase();
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,4 +86,26 @@ class _HomeLayoutScreenState extends State<HomeLayoutScreen> {
   {
     return 'Ahmed Ali';
   }
-}
+  void createDatabase()async
+  {
+   var database=await openDatabase(
+     'todo.db',
+     version: 1,
+     onCreate: (database,version)
+       {
+         print('database crated');
+         database.execute( 'CREAT TABLE(id INTEGER PRIMARY KEY, title TEXT ,date TEXT,time TEXT,status TEXT)').then((value){
+           print('table created');
+         }).catchError((error){
+           print('error when created table ${error.toString()}');
+         });
+       },
+     onOpen: (database){
+       print('database opened');
+     },
+   );
+  }
+
+
+  }
+
